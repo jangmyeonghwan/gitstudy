9bf967f5a8a9bd6b485185f5703fab0f05db484f
jun0925 <31940359+jun0925@users.noreply.github.com>
2021-08-19 20:56:11 +0900

    fifth commit

--- index.php
+++ index.php
@@ -10,6 +10,8 @@
     <h1>Git Study</h1>
     <h2>1. Git 이란 무엇인가?</h2>
     <p>Git이란 분산형 버전 관리 시스템(Version Control System) 의 한 종류이며, 빠른 수행 속도에 중점을 둔다.</p>
-    <hr />  
+    <hr />
+
+    <h2>2. 분산형 시스템이란?</h2>
 </body>
 </html>
